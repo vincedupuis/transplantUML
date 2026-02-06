681349e8d2fd6b80bfca72e14cb07647ab0aa9e2
VincentD <vincent.dupuis@zimmerbiomet.com>
2026-02-06 14:57:30 -0500

    unknown

--- cmd/tpuml.go
+++ cmd/tpuml.go
@@ -16,19 +16,19 @@ func main() {
 	flag.Parse()
 
 	scxml := readFile(inputFileName)
-	stateMachine := convertScxmlToStateMachineModel(scxml)
+	stateMachineModel := convertScxmlToStateMachineModel(scxml)
 
 	if *exportFileName != "" {
-		exportToJson(stateMachine, exportFileName)
+		exportStateMachineModelToJson(stateMachineModel, exportFileName)
 	}
 
 	tmplFile := readFile(tmplFileName)
-	output := runTemplate(stateMachine, tmplFile)
+	output := runTemplate(stateMachineModel, tmplFile)
 	writeFile(outputFileName, output)
 	fmt.Printf("Output file successfully written to: %s\n", *outputFileName)
 }
 
-func exportToJson(stateMachine *internal.StateMachine, exportFileName *string) {
+func exportStateMachineModelToJson(stateMachine *internal.StateMachine, exportFileName *string) {
 	marshal, err := json.MarshalIndent(stateMachine, "", "  ")
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error exporting state machine: %s", err)
